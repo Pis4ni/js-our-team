cbee6d38a908828d374aac9d253d05f2d807bc59
Pis4ni <pis4wrk@gmail.com>
2023-08-24 15:27:42 +0200

    add: milestone 0

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// Creare l’array di oggetti con le informazioni fornite.
+const team = [
+    {   
+        name:'Wayne',
+        surname:'Barnett',
+        fullName:'Wayne Barnett',
+        role:'Founder & CEO',
+        img:'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name:'Angela',
+        surname:'Caroll',
+        fullName:'Angela Caroll',
+        role:'Chief Editor',
+        img:'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name:'Walter',
+        surname:'Gordon',
+        fullName:'Walter Gordon',
+        role:'Office Manager',
+        img:'walter-gordon-office-manager.jpg',
+    },
+    {
+        name:'Angela',
+        surname:'Lopez',
+        fullName:'Angela Lopez',
+        role:'Social Media Manager',
+        img:'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name:'Scott',
+        surname:'Estrada',
+        fullName:'Scott Estrada',
+        role:'Developer',
+        img:'scott-estrada-developer.jpg',
+    },
+    {
+        name:'Barbara',
+        surname:'Ramos',
+        fullName:'Barbara Ramos',
+        role:'Graphic Designer',
+        img:'barbara-ramos-graphic-designer.jpg',
+    },
+
+]
